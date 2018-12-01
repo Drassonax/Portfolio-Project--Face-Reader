82999d1e0cbacc17064dad104c9f2e9e4c16cc61
Hrvoje <hvujnovic@gmail.com>
2018-12-01 15:42:42 +0100

    Heroku optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,9 @@ if (process.env.NODE_ENV === 'test') {
 } else if (process.env.NODE_ENV === 'development') {
     require('dotenv').config({ path: '.env.development' })
 }
-*/
+
 require('dotenv').config({ path: '.env.development' })
+*/
 
 module.exports = (env) => {
     const isProduction = env === 'production'
@@ -45,12 +46,7 @@ module.exports = (env) => {
         },
         plugins: [
             new webpack.DefinePlugin({
-                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+                'process.env.CLARIFAI_API_KEY': JSON.stringify(process.env.CLARIFAI_API_KEY)
               })
         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
